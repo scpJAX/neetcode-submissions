46e2437c9db689e1056b024b0dde290415b231c4
scpJAX <110111024+scpJAX@users.noreply.github.com>
2026-06-27 21:32:36 -0400

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> p;
+        for (int i = 0; i < s.size(); i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+                p.push(s[i]);
+            }
+            else {
+                if(s[i] == ')' && (p.size() > 0 && p.top() == '(')){
+                    p.pop();
+                }
+                else if (s[i] == ']' && (p.size() > 0 && p.top() == '[')){
+                    p.pop();
+                }
+                else if (s[i] == '}' && (p.size() > 0 && p.top() == '{')){
+                    p.pop();
+                }
+                else {
+                    return false;
+                }
+            } 
+        }
+        return p.size() == 0;
+    }
+};

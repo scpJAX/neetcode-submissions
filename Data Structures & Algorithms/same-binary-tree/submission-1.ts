ee8a593b30389a1971f056ac97af86f82f57b4ba
scpJAX <110111024+scpJAX@users.noreply.github.com>
2026-07-05 13:36:18 -0400

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {boolean}
+     */
+    isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+        if(p === null && q === null){
+            return true; //we've hit the end of the
+        }
+        else if ((p === null && q !== null) || (p !== null && q === null)){
+            return false; //not same structure
+        }
+        else {
+            if( p.val !== q.val){
+                return false;
+            }
+            else if ((p.left !== null && q.left === null) || (p.left === null && q.left !== null)){
+                return false;
+            }
+            else if ((p.right !== null && q.right === null) || (p.right === null && q.right !== null)){
+                return false;
+            }
+            else {
+                return (this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right));
+            }
+        }
+    }
+}

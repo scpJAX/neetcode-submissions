be2195c757b540447fdd49573bbdc8e5ce55b71a
scpJAX <110111024+scpJAX@users.noreply.github.com>
2026-07-05 12:07:58 -0400

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.ts
@@ -0,0 +1,26 @@
+
+//Definition for a binary tree node.
+// class TreeNode {
+//     val: number = 0;
+//     left: TreeNode = null;
+//     right: TreeNode = null;
+//     constructor(val = 0, left = null, right = null) {
+//     this.val = val;
+//     this.left = left;
+//     this.right = right;
+//     }
+// }
+ 
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root: TreeNode | null): number {
+        if(!root)
+            return 0;
+        
+        return (1+Math.max(this.maxDepth(root.left), this.maxDepth(root.right)))
+    }
+}

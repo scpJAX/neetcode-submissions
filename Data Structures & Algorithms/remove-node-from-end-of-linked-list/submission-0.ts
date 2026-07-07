73a442a3f881fcc08e1e2d9ad1bc16fc14e9b959
scpJAX <110111024+scpJAX@users.noreply.github.com>
2026-07-07 18:56:16 -0400

    Add: remove-node-from-end-of-linked-list - submission-0

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-0.ts
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} n
+     * @return {ListNode}
+     */
+    removeNthFromEnd(head: ListNode | null, n: number): ListNode {
+        let slow = head;
+        let fast = head;
+        for(let i = 0; i <= n; i++){
+            //traverse fast n+1 steps 
+            if(fast === null && i <= n){
+                //we are removing the first node
+                if (i > 0){
+                    return head.next
+                } else {
+                    return null;
+                }
+            }
+            else {
+                fast = fast.next
+            }
+        }
+
+        while(fast !== null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+
+        return head;
+    }
+}

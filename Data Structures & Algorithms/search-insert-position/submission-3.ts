83e9236f28d6b8d6841b6550ef2be5a1c9897d10
scpJAX <110111024+scpJAX@users.noreply.github.com>
2026-07-08 15:15:08 -0400

    Add: search-insert-position - submission-3

---
+++ Data Structures & Algorithms/search-insert-position/submission-3.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums: number[], target: number): number {
+        let left = 0;
+        let right = nums.length -1;
+
+        while (left <= right){
+            let mid = Math.trunc((left+right) / 2);
+            if (nums[mid] === target){
+                return nums.findIndex(a => a === target);
+            } else{
+                if (nums[mid] < target){
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        //edge cases for either most side of array
+        return left;
+    }
+}
